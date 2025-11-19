a1cf02c44431b8d3d9469e4568d584f695081482
doncato <doncato@notarobot.cyou>
2025-11-19 11:28:33 +0100

    Improve find page

--- src/views/FindView.vue
+++ src/views/FindView.vue
@@ -3,14 +3,15 @@
     <h1>Finding...</h1>
     <p>Simply type to search. Searching for:</p>
     <ul>
-      <li v-for="(string, index) in decodedStrings" :key="index">{{ string }}</li>
+      <li v-for="(value, key) in searchStrings.value" :class="{ found: value.matches }" :key="key">{{ key }}</li>
     </ul>
     <button @click="goToHomePage">Go Back</button>
+    <button @click="resetState">Reset</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   data() {
@@ -19,27 +20,58 @@ export default defineComponent({
     };
   },
   computed: {
-    decodedStrings(): string[] {
+    searchStrings(): Record {
       const strings = this.$route.query.strings as string;
       if (strings) {
-        return strings.split(',').map(decodeURIComponent);
+        let strarr = strings.split(',').map(decodeURIComponent) as string[];
+        console.log(strarr);
+        return ref(strarr
+          .reduce((acc, curr) => {
+            console.log(curr)
+            acc[curr] = { matches: false };
+            return acc;
+          }, {} as Record));
       }
-      const searchStrings = strings.map(v => v.toLowerCase());
       return [];
+    },
+    caseSensitive(): bool {
+      return this.$route.query.case as bool;
+    },
+    sticky(): bool{
+      return this.$route.query.sticky as bool;
     }
   },
   methods: {
     goToHomePage(): void {
       this.$router.push({ name: 'Home' });
     },
+    actFound(key: string, index: number): void {
+      this.searchStrings.value[key]["matches"] = true;
+      var audio = new Audio("public/trigger-sound.wav");
+      audio.play();
+      if (!this.sticky) {
+        this.inputSequence = this.inputSequence.slice(0, index) + this.inputSequence.slice(index+1);
+      }
+    },
+    resetState(): void {
+      for (var [key, value] of Object.entries(this.searchStrings.value)) {
+        value["matches"] = false;
+      }
+      this.inputSequence = "";
+    },
     handleKeyPress(event: KeyboardEvent): void {
-      this.inputSequence += event.key;
+      if (this.caseSensitive) {
+        this.inputSequence += event.key;
+      } else {
+        this.inputSequence += event.key.toLowerCase();
+      }
 
-      console.log(this.inputSequence);
-      console.log(this.searchStrings.value);
-
-      if (this.searchStrings.some(v => this.inputSequence.includes(v))) {
-        console.log("Found");
+      for (var [key, value] of Object.entries(this.searchStrings.value)) {
+        let indx =this.inputSequence.search(key);
+        value["matches"] = indx >= 0;
+        if (indx >= 0) {
+          this.actFound(key, indx);
+        }
       }
 
       if (this.inputSequence.length > 255) {
@@ -48,11 +80,11 @@ export default defineComponent({
       }
     },
   },
-  created: function() {
+  mounted: function() {
       window.addEventListener('keypress', this.handleKeyPress);
   },
-  destroyed: function() {
-      window.addEventListener('keypress', this.handleKeyPress);
+  unmounted: function() {
+      window.removeEventListener('keypress', this.handleKeyPress);
   },
 });
 </script>
